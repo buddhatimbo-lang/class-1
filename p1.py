398363d4cd84558910a8215f685a088e61470ace
buddhatimbo-lang <buddhatimbo@gmail.com>
2026-04-24 12:33:58 +0100

    first commit

---
+++ p1.py
@@ -0,0 +1,21 @@
+from operator import truediv
+
+age = 105
+first_name = ("DRACULA")
+height= "5.10"
+is_student = truediv(18,5.10)
+print("this is my age:",age)
+print("this is my first_name:",first_name)
+print("this is my height:",height)
+print("this is my is_student:",is_student)
+
+age= int ("18") #cast means to change data type to another
+print
+
+
+
+
+
+
+
+
